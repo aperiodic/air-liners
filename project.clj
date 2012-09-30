b7480228b2939331e9d995556e882fd4e04b1cc3
Dan Lidral-Porter <dlp@aperiodic.org>
2012-09-30 14:25:35 -0700

    Oops, forget project.clj somehow

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject air-liners "0.1.0-SNAPSHOT"
+  :description "3D-printed algorithmic sculpture with quil"
+  :url "http://github.com/aperiodic/air-liners"
+  :license {:name "GNU General Public License"
+            :url "http://www.gnu.org/licenses/gpl.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [quil "1.6.0"]])
